daed33fb9e1181911cab828510ff4953b6848a5d
J. Michael Dean <mike.dean@hsc.utah.edu>
2015-06-14 17:15:18 -0600

    Initial Commit

---
+++ Camelot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camelot
+//
+//  Created by J Michael Dean on 6/14/15.
+//  Copyright © 2015 J Michael Dean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
